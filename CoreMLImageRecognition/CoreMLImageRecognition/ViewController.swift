b55a020b9b82960914b18c373ab7fca6c7650172
Wang, Terry <terry.wang@accenture.com>
2017-06-16 23:42:15 -0400

    hooked up actions

--- CoreMLImageRecognition/CoreMLImageRecognition/ViewController.swift
+++ CoreMLImageRecognition/CoreMLImageRecognition/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var classifier: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
+    // MARK: IB Actions
+    @IBAction func camera(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func openLibrary(_ sender: Any) {
+        
+    }
+    
 }
 
+
+extension ViewController {
+    
+    
+    
+}
